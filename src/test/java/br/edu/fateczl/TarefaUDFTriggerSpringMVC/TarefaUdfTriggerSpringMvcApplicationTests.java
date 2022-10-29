bd63691ac80331a6ddb1093f7639dec023ab90e2
ThaisScheiner <thais.silva124@fatec.sp.gov.br>
2022-10-29 19:55:16 -0300

    primeiro commit
    tarefa udf spring

---
+++ src/test/java/br/edu/fateczl/TarefaUDFTriggerSpringMVC/TarefaUdfTriggerSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.fateczl.TarefaUDFTriggerSpringMVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarefaUdfTriggerSpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
